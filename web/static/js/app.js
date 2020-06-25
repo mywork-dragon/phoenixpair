be4f9d2eece0ac0226d038e93b3b4e6d427a3af9
mywork-dragon <dragonblueyounger@gmail.com>
2020-06-25 10:27:10 +1000

    add header to route config

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,64 +1,40 @@
-// import React                    from 'react';
-// import ReactDOM                 from 'react-dom';
-// import { browserHistory }       from 'react-router';
-// import { syncHistoryWithStore } from 'react-router-redux';
-// import configureStore           from './store';
-// import Root                     from './containers/root';
-
-// const store = configureStore(browserHistory);
-// debugger;
-// const history = syncHistoryWithStore(browserHistory, store);
-// const target = document.getElementById('main_container');
-// const node = <Root routerHistory={history} store={store} />;
-
-// ReactDOM.render(node, target);
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
-import { Route } from 'react-router'
+import {
+  ConnectedRouter as Router,
+  routerMiddleware
+} from 'react-router-redux'
+import {
+  Route,
+  Link
+} from 'react-router-dom'
 
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
 
-import reducers from './reducers' // Or wherever you keep your reducers
-import MainLayout                   from './layouts/main';
+import configRoutes from './routes'
+import App from './views/registrations/new'
+import AnotherApp from './views/sessions/new'
+import rootReducer from './reducers'
 
-import RegistrationsNew             from './views/registrations/new';
-import SessionsNew                  from './views/sessions/new';
-
-// Create a history of your choosing (we're using a browser history in this case)
 const history = createHistory()
+const rMiddleware = routerMiddleware(history)
 
-// Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history)
-
-// Add the reducer to your store on the `router` key
-// Also apply our middleware for navigating
 const store = createStore(
-  combineReducers({
-    ...reducers,
-    router: routerReducer
-  }),
-  applyMiddleware(middleware)
+  rootReducer,
+  applyMiddleware(thunk, rMiddleware)
 )
 
-// Now you can dispatch navigation actions from anywhere!
-// store.dispatch(push('/foo'))
 
 ReactDOM.render(
   <Provider store={store}>
-    { /* ConnectedRouter will use the store from Provider automatically */ }
-    <ConnectedRouter history={history}>
+    <Router history={history}>
       <div>
-        
-        <Route path="/sign_up" component={RegistrationsNew} />
-        <Route path="/sign_in" component={SessionsNew} />
+        {configRoutes(store)}
       </div>
-    </ConnectedRouter>
+    </Router>
   </Provider>,
   document.getElementById('main_container')
-)
+);
--- web/static/js/routes/index.js
+++ web/static/js/routes/index.js
@@ -1,37 +1,34 @@
-import { IndexRoute}        from 'react-router';
-import { Switch, Route } from 'react-router-dom'
 import React                        from 'react';
+import { Route, Redirect }          from 'react-router-dom'
 import MainLayout                   from '../layouts/main';
-import AuthenticatedContainer       from '../containers/authenticated';
-import ChallengesIndex              from '../views/challenges/index';
-import ChallengesShow               from '../views/challenges/show';
+import App                          from '../containers/app';
 import RegistrationsNew             from '../views/registrations/new';
 import SessionsNew                  from '../views/sessions/new';
+import ChallengesIndex              from '../views/challenges/index';
 import Actions                      from '../actions/sessions';
 
 export default function configRoutes(store) {
-  const _ensureAuthenticated = (nextState, replace, callback) => {
-    const { dispatch } = store;
-    const { session } = store.getState();
-    const { currentUser } = session;
-
-    if (!currentUser && localStorage.getItem('phoenixAuthToken')) {
-      dispatch(Actions.currentUser());
-    } else if (!localStorage.getItem('phoenixAuthToken')) {
-      replace("/sign_in")
-    }
-    callback();
-  };
   return (
-    <Route component={MainLayout}>
+    <div>
+      <h1>Phoenix Pair</h1>
+      <Route exact path="/" component={App} />
       <Route path="/sign_up" component={RegistrationsNew} />
       <Route path="/sign_in" component={SessionsNew} />
-
-      <Route path="/" component={AuthenticatedContainer} onEnter={_ensureAuthenticated}>
-        <IndexRoute component={ChallengesIndex} />
-        <Route path="/challenges/:id" component={ChallengesShow} />
-      </Route>
-    </Route>
+      <AuthenticatedRoute path="/challenges" component={ChallengesIndex} />
+    </div>
   );
 }
+
+const AuthenticatedRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    localStorage.getItem('phoenixAuthToken') ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/sign_in',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
 
